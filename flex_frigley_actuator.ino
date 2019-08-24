2fcd66090e386cfaa9ad78ac7f404e796b7d9feb
vkalarocks <53038647+vkalarocks@users.noreply.github.com>
2019-08-24 18:27:54 +0530

    Updated

--- flex_frigley_actuator.ino
+++ flex_frigley_actuator.ino
@@ -1,11 +1,14 @@
-//for 35.40 flex
+//for 35.40 flex, CHANGE THE READINGS ACCORDINGLY
 int a;
+
 void setup()
 {
   Serial.begin(2400);
   pinMode(A1, INPUT);
   pinMode(5,OUTPUT);
   pinMode(6,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
   
 }
 void loop()
@@ -19,20 +22,28 @@ delay(500);
  */
 //5 input
 //6 input
- if(a>145)
+//3 input
+//4 input
+ if(a>145 )
  {
   digitalWrite(5, LOW);
   digitalWrite(6, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
  }
  else if(a>=118 && a<130)
  {
   digitalWrite(5, HIGH);
   digitalWrite(6,LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(4,LOW);
  }
  else
  {
   digitalWrite(5,LOW);
   digitalWrite(6,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
  }
 
 }
