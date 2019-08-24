3d137aa63e85241883446fb75d26322091a5dbb7
vkalarocks <53038647+vkalarocks@users.noreply.github.com>
2019-08-24 19:08:49 +0530

    Updated

--- flex_frigley_actuator.ino
+++ flex_frigley_actuator.ino
@@ -1,4 +1,4 @@
-//for 35.40 flex, CHANGE THE READINGS ACCORDINGLY
+//for 35.40 flex
 /* front bend (>145)
  *  back bend ( < )
  *  normal(118-129)
@@ -7,16 +7,21 @@
 //6 input
 //3 input
 //4 input
+#define T A1
+#define U 5
+#define I 6
+#define O 3
+#define P 4
 int a;
 
 void setup()
 {
   Serial.begin(2400);
-  pinMode(A1, INPUT);
-  pinMode(5,OUTPUT);
-  pinMode(6,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
+  pinMode(T, INPUT);
+  pinMode(U,OUTPUT);
+  pinMode(I,OUTPUT);
+  pinMode(O,OUTPUT);
+  pinMode(P,OUTPUT);
   
 }
 void loop()
@@ -24,34 +29,27 @@ void loop()
 a= analogRead(A1);
 Serial.println(a);
 delay(500);
-/* front bend (>145)
- *  back bend ( < )
- *  normal(118-129)
- */
-//5 input
-//6 input
-//3 input
-//4 input
+
  if(a>145 )
  {
-  digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);
-  digitalWrite(3, LOW);
-  digitalWrite(4, HIGH);
+  digitalWrite(U, LOW);
+  digitalWrite(I, HIGH);
+  digitalWrite(O, LOW);
+  digitalWrite(P, HIGH);
  }
  else if(a>=118 && a<130)
  {
-  digitalWrite(5, HIGH);
-  digitalWrite(6,LOW);
-  digitalWrite(3, HIGH);
-  digitalWrite(4,LOW);
+  digitalWrite(U, HIGH);
+  digitalWrite(I,LOW);
+  digitalWrite(O, HIGH);
+  digitalWrite(P,LOW);
  }
  else
  {
-  digitalWrite(5,LOW);
-  digitalWrite(6,LOW);
-  digitalWrite(3,LOW);
-  digitalWrite(4,LOW);
+  digitalWrite(U,LOW);
+  digitalWrite(I,LOW);
+  digitalWrite(O,LOW);
+  digitalWrite(P,LOW);
  }
 
 }
