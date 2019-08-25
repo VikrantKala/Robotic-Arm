ba9c3ab176722418346ec2088c158bc7a68f5b8a
vkalarocks <53038647+vkalarocks@users.noreply.github.com>
2019-08-25 05:31:34 +0530

    Updated

--- flex_Servo(HS-785HB).ino
+++ flex_Servo(HS-785HB).ino
@@ -2,12 +2,12 @@
 #include<Servo.h>
 int q,w;
 int r=1472;
-Servo s; 
+Servo s2; 
 void setup()
 {
   Serial.begin(2400);
-  s.attach(flex_servo_pin);
-  s.writeMicroseconds(r);
+  s2.attach(flex_servo_pin);
+  s2.writeMicroseconds(r);
   w=f2;
   
 }
@@ -25,13 +25,13 @@ delay(100);
 if(q>w+45 && r<1592)
 {
   r=r+5;
-s.writeMicroseconds(r);
+s2.writeMicroseconds(r);
     delay(20);
 }
 else if(q>=w-5 && q<=w+5 && r>1472)
 {
   r=r-5;
-  s.writeMicroseconds(r);
+  s2.writeMicroseconds(r);
     delay(20);
 }
 }
