99d036294e7e470bd9f950884fe5578eeed7c28a
vkalarocks <53038647+vkalarocks@users.noreply.github.com>
2019-08-29 02:57:37 +0530

    Completed

--- Transmission.ino
+++ Transmission.ino
@@ -1,1 +1,79 @@
+#include <RF24.h>
+#include<Wire.h>
 
+#define flex_pin_1 A1
+#define flex_pin_2 A2
+
+RF24 radio(7, 8);
+
+byte addresses[][6] = {"", "2Node"};
+const int MPU1=0x68,MPU2=0x69;  
+int AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ,x,y,z;
+int x1,y1,x2,y2;
+int minVal=265, maxVal=402, data;
+
+int v[6]; 
+
+void setup() {
+  
+  radio.begin();
+  radio.setPALevel(RF24_PA_MIN);
+  
+  radio.openWritingPipe(addresses[1]);
+
+  Wire.begin(); 
+  Wire.beginTransmission(MPU1); 
+  Wire.write(0x6B); 
+  Wire.write(0); 
+  Wire.endTransmission(true); 
+  
+  Wire.begin(); 
+  Wire.beginTransmission(MPU2); 
+  Wire.write(0x6B); 
+  Wire.write(0); 
+  Wire.endTransmission(true); 
+  Serial.begin(9600); 
+  } 
+ 
+  void loop(){ 
+    Serial.print("First\n");
+    angles(MPU1, x1, y1);
+    Serial.println("--------------------");
+
+    Serial.print("Second \n");
+    angles(MPU2, x2, y2);
+    Serial.print("----------------------------\n");
+    
+    v[0]=x1;v[1]=y1;v[2]=x2;v[3]=y2;
+
+    v[4]=analogRead(flex_pin_1);
+    v[5]=analogRead(flex_pin_2);
+    
+    radio.stopListening();
+    if(radio.available())
+    {
+      radio.write(&v, sizeof(v));
+    }
+    delay(1000);
+}
+
+void angles(const int MPU, int &a, int &b)
+  {
+    Wire.beginTransmission(MPU); 
+    Wire.write(0x3B); 
+    Wire.endTransmission(false); 
+    Wire.requestFrom(MPU,14,true); 
+    AcX=Wire.read()<<8|Wire.read(); 
+    AcY=Wire.read()<<8|Wire.read(); 
+    AcZ=Wire.read()<<8|Wire.read(); 
+    int xAng = map(AcX,minVal,maxVal,-90,90); 
+    int yAng = map(AcY,minVal,maxVal,-90,90); 
+    int zAng = map(AcZ,minVal,maxVal,-90,90);
+
+x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI); 
+y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI); 
+z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+
+a=x;
+b=y;
+ }
