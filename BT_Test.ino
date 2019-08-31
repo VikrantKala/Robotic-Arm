44c1d2d25578c41a316cab37346a05d113b22675
vkalarocks <53038647+vkalarocks@users.noreply.github.com>
2019-08-31 16:13:51 +0530

    Update BT_Test.ino

--- BT_Test.ino
+++ BT_Test.ino
@@ -1,14 +1,28 @@
-int v[6]={1,2,3,4,5,6};
-
+int v[6]={1,2,3,4,5,6},p=0;
 void setup() {
-  Serial.begin(9600);
-
-
+ Serial.begin(9600);
 }
-void loop(){
 
-  for(int i=0;i<6;i++)
-    {
-    v[i]=Serial.read();
-    }
+void loop() {
+ if(p==0)
+    Serial.write(v[0]); 
+    else if(p==1)
+    Serial.write(v[1]);
+    else if(p==2)
+    Serial.write(v[2]);
+    else if(p==3)
+    Serial.write(v[3]); 
+    else if(p==4)
+    Serial.write(v[4]);
+    else if(p==5)
+    Serial.write(v[5]);
+    
+    p++;
+
+   if(p>6)
+    p=0;
+    
+    
+    delay(20);
+
 }
